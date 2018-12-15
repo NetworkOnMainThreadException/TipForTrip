5d61bd9d8b92741c47b0d0c2af9f3cf6f70c9cf0
pinizhegorodov <pinizhegorodov@edu.hse.ru>
2018-12-16 02:02:39 +0300

    Add plus button

--- app/src/main/java/networkonmainthreadexception/tipfortrip/MapFragment.java
+++ app/src/main/java/networkonmainthreadexception/tipfortrip/MapFragment.java
@@ -10,9 +10,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
 
@@ -36,4 +37,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
 }
--- app/src/main/java/networkonmainthreadexception/tipfortrip/TabsFragment.kt
+++ app/src/main/java/networkonmainthreadexception/tipfortrip/TabsFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_tabs.*
-import networkonmainthreadexception.tipfortrip.R.id.root
 
 
 
@@ -17,6 +16,7 @@ class TabsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         return inflater.inflate(R.layout.fragment_tabs, container, false)
     }
@@ -27,6 +27,9 @@ class TabsFragment : Fragment() {
             .add(MapFragment(), "Карта")
             .add(ProfileFragment(), "Профиль")
             .build(childFragmentManager)
+        Plus.setOnClickListener {
+            
+        }
     }
 
 }
