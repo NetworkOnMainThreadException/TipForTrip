d0cea92622e209dbf9453cea5d6080d0d5ee241d
pavel <p97p@ya.ru>
2018-12-15 23:11:03 +0300

    up gradle

--- app/src/main/java/networkonmainthreadexception/tipfortrip/TabsFragment.kt
+++ app/src/main/java/networkonmainthreadexception/tipfortrip/TabsFragment.kt
@@ -7,9 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_tabs.*
-import networkonmainthreadexception.tipfortrip.R.id.root
-
-
 
 
 class TabsFragment : Fragment() {
