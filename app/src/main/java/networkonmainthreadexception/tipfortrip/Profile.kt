78c0d32afc109061fd4af982dbe34b8993e68910
antonsavacenko <avsavachenko@edu.hse.ru>
2018-12-16 08:38:10 +0300

    finally finally v1

--- app/src/main/java/networkonmainthreadexception/tipfortrip/Profile.kt
+++ app/src/main/java/networkonmainthreadexception/tipfortrip/Profile.kt
@@ -4,5 +4,6 @@ class Profile(
     var name: String = "",
     var surname: String = "",
     var phone: String = "",
-    var cityId: String = ""
+    var cityId: String = "",
+    var avatar: String =""
 ) : Model()
