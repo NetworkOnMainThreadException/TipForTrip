0e277cb2d76f9f16fd87651d77150d37ca081249
pavel <p97p@ya.ru>
2018-12-16 04:40:42 +0300

    pick date

--- app/src/main/java/networkonmainthreadexception/tipfortrip/UiUtils.kt
+++ app/src/main/java/networkonmainthreadexception/tipfortrip/UiUtils.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import android.app.DatePickerDialog
+import java.util.*
+
 
 fun Context.showToast(text: String) {
     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
@@ -21,4 +24,19 @@ fun pushFragment(fragmentManager: FragmentManager, fragment: Fragment) {
         .replace(R.id.root, fragment)
         .addToBackStack(null)
         .commit()
+}
+
+fun pickDateUntil(context: Context, listener: (Date) -> Unit, maxDate: Date) {
+    val c = Calendar.getInstance() // default date
+    val dialog = DatePickerDialog(
+        context, 0,
+        { _, y, m, d -> listener(GregorianCalendar(y, m, d).time) },
+        c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)
+    )
+    dialog.datePicker.maxDate = maxDate.getTime()
+    dialog.show()
+}
+
+fun pickDateUntilNow(context: Context, listener: (Date) -> Unit) {
+    pickDateUntil(context, listener, Calendar.getInstance().time)
 }
