61ea69e3a84871b8c698cf046592f7c4c522389c
pavel <p97p@ya.ru>
2018-12-16 04:03:35 +0300

    bottom navigation https://droidmentor.com/bottomnavigationview-with-viewpager-android/

--- app/src/main/java/networkonmainthreadexception/tipfortrip/TabsFragment.kt
+++ app/src/main/java/networkonmainthreadexception/tipfortrip/TabsFragment.kt
@@ -3,12 +3,12 @@ package networkonmainthreadexception.tipfortrip
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_tabs.*
-
-
+import androidx.viewpager.widget.ViewPager
 
 
 class TabsFragment : Fragment() {
@@ -16,20 +16,47 @@ class TabsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View? {
         return inflater.inflate(R.layout.fragment_tabs, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        TabsBuilder(viewpager, tabs)
-            .add(EventsFragment(), "Маршруты")
-            .add(MapFragment(), "Карта")
-            .add(ProfileFragment(), "Профиль")
-            .build(childFragmentManager)
-        Plus.setOnClickListener {
-            pushFragment(fragmentManager!!, RouteCreationFragment());
+
+        viewPager.adapter = ViewPagerAdapter(
+            fragmentManager,
+            listOf(EventsFragment(), MapFragment(), ProfileFragment()),
+            listOf("Маршруты", "Карта", "Профиль")
+        )
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            viewPager.currentItem = when (it.itemId) {
+                R.id.events -> 0
+                R.id.map -> 1
+                R.id.profile -> 2
+                else -> -1
+            }
+            false
         }
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            private var prevMenuItem: MenuItem? = null
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                (prevMenuItem ?: bottomNavigation.menu.getItem(0)).isChecked = false
+                bottomNavigation.menu.getItem(position).isChecked = true
+                prevMenuItem = bottomNavigation.menu.getItem(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+        })
     }
 
 }
